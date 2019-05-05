e52271a7389e4cd0f83b9363dda1e7cd31509f70
ThomasAnkcorn <thomasankcorn@gmail.com>
2019-05-05 15:34:03 +0100

    Added database setup

---
+++ DB/Dockerfile
@@ -0,0 +1,3 @@
+FROM postgres
+ENV POSTGRES_DB my_database
+COPY schema.sql /docker-entrypoint-initdb.d/
---
+++ DB/schema.sql
@@ -0,0 +1,9 @@
+create schema rtm;
+
+create table rtm.user (
+  id               serial primary key,
+  first_name       text not null check (char_length(first_name) < 80),
+  last_name        text check (char_length(last_name) < 80),
+  about            text,
+  created_at       timestamp default now()
+);
