6f46af8bf900c9f9b57feea4da5200e99c67f4e2
ThomasAnkcorn <thomasankcorn@gmail.com>
2019-05-06 21:07:19 +0100

    Added Schema base

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,3 +1,7 @@
+begin;
+
+drop schema if exists rtm;
+
 create schema rtm;
 
 create table rtm.user (
@@ -7,3 +11,12 @@ create table rtm.user (
   about            text,
   created_at       timestamp default now()
 );
+
+comment on table rtm.user is 'A User';
+comment on column rtm.user.id is 'The primary unique identifier for the user.';
+comment on column rtm.user.first_name is 'The user’s first name.';
+comment on column rtm.user.last_name is 'The user’s last name.';
+comment on column rtm.user.about is 'A short description about the user, written by the user.';
+comment on column rtm.user.created_at is 'The time this user was created.';
+
+commit;
