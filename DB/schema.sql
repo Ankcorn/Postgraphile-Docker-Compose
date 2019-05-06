b25e44d7460bd0e9c31c2932f92537711ea2dabb
ThomasAnkcorn <thomasankcorn@gmail.com>
2019-05-06 22:00:10 +0100

    added account registration

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,8 +1,10 @@
 begin;
 
 drop schema if exists rtm;
+drop schema if exists rtm_private;
 
 create schema rtm;
+create schema rtm_private;
 
 create table rtm.user (
   id               serial primary key,
@@ -18,5 +20,37 @@ comment on column rtm.user.first_name is 'The user’s first name.';
 comment on column rtm.user.last_name is 'The user’s last name.';
 comment on column rtm.user.about is 'A short description about the user, written by the user.';
 comment on column rtm.user.created_at is 'The time this user was created.';
+
+create table rtm_private.account (
+  user_id         integer primary key references rtm.user(id) on delete cascade,
+  email           text not null unique check (email ~* '^.+@.+\..+$'),
+  password_hash   text not null
+);
+
+comment on table rtm_private.account is 'Private information associated with a user account';
+comment on column rtm_private.account.user_id is 'The primary key for a user';
+comment on column rtm_private.account.email is 'The email of the user';
+comment on column rtm_private.account.password_hash is 'The hashed password of the user account';
+
+create extension if not exists "pgcrypto";
+
+create function rtm.register_person (
+    first_name text,
+    email text,
+    password text
+) returns rtm.user as $$
+declare
+  person rtm.user;
+begin
+  insert into rtm.user (first_name) values
+    (first_name)
+    returning * into person;
+
+  insert into rtm_private.account (user_id, email, password_hash) values 
+    (person.id, email, crypt(password, gen_salt('bf')));
+
+  return person;
+end;
+$$ language plpgsql strict security definer;
 
 commit;
